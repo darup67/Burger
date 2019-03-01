cfccec24b982905bbfabd031ceed51994d6670ad
Dhruv Patel <darup67@gmail.com>
2019-02-28 19:35:12 -0500

    burger

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Le Pigeon Burger', '0');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Le Pigeon Burger', '1');
 INSERT INTO burgers (burger_name, devoured) VALUES ('Whiskey King Burger', '1');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Chargrilled Burger with Roquefort Cheese', '0');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chargrilled Burger with Roquefort Cheese', '1');
