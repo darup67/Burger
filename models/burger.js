f0773b11b8def0fec5c3bbbc3abc4560fd349e38
Dhruv Patel <darup67@gmail.com>
2019-03-02 21:06:17 -0500

    burger

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 var orm = require("../config/orm.js");
 var path = require('path');
 
-var db = require("../models");
+// var db = require("./models/burger.js");
 
 var burger = {
     selectAll: function(cb) {
