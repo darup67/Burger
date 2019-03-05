cd6189451480ade0e164c6d369de0165b63da85d
Dhruv Patel <darup67@gmail.com>
2019-03-04 22:58:35 -0500

    burger

--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,7 @@ var connection = require("./connection.js");
 
 var orm = {
     selectAll: function(tableName, bg) {
-        var queryString = "SELECT * FROM ??";
+        var queryString = "SELECT * FROM burgers";
         console.log(queryString);
         connection.query(queryString, function(err, result) {
             if (err) {
